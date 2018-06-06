8843dfa0641a8dcdba02ed0870a2d4da4f426a4b
Guilherme Uberti <guilherme.uberti@hotmail.com>
2018-06-06 11:12:11 -0300

    Projeto Criado

---
+++ src/main/java/com/senai/projetoescola/ProjetoEscolaApplication.java
@@ -0,0 +1,12 @@
+package com.senai.projetoescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEscolaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/senai/projetoescola/ProjetoEscolaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.senai.projetoescola;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoEscolaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
